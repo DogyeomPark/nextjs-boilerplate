424b23949a43c6d2673516bf82f0f06f0368422d
김윤수 <oonoo@memecore.org>
2024-03-19 07:13:06 +0000

    Development

--- src/app/api/siwe/nonce/route.ts
+++ src/app/api/siwe/nonce/route.ts
@@ -5,11 +5,14 @@ import { apiService } from '@/lib/apiService';
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
-  const resp = await apiService.user.userControllerGetNonce();
+  const resp = await apiService.wallet.userControllerGetNonce();
   const nonce = resp.data.nonce;
 
-  return NextResponse.json(
-    { nonce },
-    { headers: { 'Set-Cookie': resp.headers.getSetCookie()[0] } }
-  );
+  const cookie = resp.headers.get('set-cookie');
+
+  if (!cookie) {
+    return NextResponse.error();
+  }
+
+  return NextResponse.json({ nonce }, { headers: { 'Set-Cookie': cookie } });
 }
