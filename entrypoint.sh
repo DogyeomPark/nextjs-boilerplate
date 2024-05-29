f1eb1bbb2f601b0393ba26cb260a189a8391f994
seonwuk <kimsw@blocksmith.xyz>
2024-05-29 13:58:06 +0900

    rollback commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,9 +20,8 @@ else
 fi
 
 # AWS Secret Manager에서 secret value 호출
-secret=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
-echo "secret >> $secret"
-eval "$(echo "$secret" | jq -r 'to_entries | .[] | "export \(.key)=\(.value)"')"
+#secret=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
+#eval "$(echo "$secret" | jq -r 'to_entries | .[] | "export \(.key)=\(.value)"')"
 
 
 pnpm build
