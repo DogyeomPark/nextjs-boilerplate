e1a7387873bdfbdc49a8281404aa8b4b06eb18fe
seonwuk <kimsw@blocksmith.xyz>
2024-05-29 13:42:17 +0900

    validate env

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,8 +20,10 @@ else
 fi
 
 # AWS Secret Manager에서 secret value 호출
-#secret=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
-#eval "$(echo "$secret" | jq -r 'to_entries | .[] | "export \(.key)=\(.value)"')"
+secret=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
+echo "secret >> $secret"
+eval "$(echo "$secret" | jq -r 'to_entries | .[] | "export \(.key)=\(.value)"')"
+
 
 pnpm build
 exec pnpm start
